8311e708f21edd73f40d55cf7ec372b405390741
sonbach317 <bachnshe161333@fpt.edu.vn>
2024-10-06 23:36:44 +0700

    update admin
    update admin template

---
+++ kms-fe/src/screens/Dashbord/Dashbord.js
@@ -0,0 +1,140 @@
+import React from "react";
+import { connect } from "react-redux";
+import ReactEcharts from "echarts-for-react";
+import "echarts-gl";
+import echarts from "echarts";
+import LogoiCON from "../../assets/images/logo-icon.svg";
+import AwsomeImage from "../../assets/images/blog/blog-page-4.jpg";
+import AwsomeImageOt from "../../assets/images/blog/blog-page-2.jpg";
+import { Dropdown } from "react-bootstrap";
+import ReferralsCard from "../../components/Dashboard/ReferralsCard";
+import ResentChat from "../../components/Dashboard/ResentChat";
+import TwitterFeedCard from "../../components/Dashboard/TwitterFeedCard";
+import FeedCards from "../../components/Dashboard/FeedsCard";
+import PageHeader from "../../components/PageHeader";
+import {
+  topProductOption,
+  topRevenueOption,
+  topRevenueMonthlyOption,
+  saleGaugeOption,
+  dataManagetOption,
+  sparkleCardData,
+} from "../../Data/DashbordData";
+import {
+  toggleMenuArrow,
+  onPressTopProductDropDown,
+  loadSparcleCard,
+  onPressReferralsDropDown,
+  onPressRecentChatDropDown,
+  onPressDataManagedDropDown,
+  facebookProgressBar,
+  twitterProgressBar,
+  affiliatesProgressBar,
+  searchProgressBar,
+} from "../../actions";
+import SparkleCard from "../../components/SparkleCard";
+
+var timer = null;
+class Dashbord extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardData: [],
+    };
+  }
+  // componentDidMount() {
+  //   window.scrollTo(0, 0);
+  //   this.loadDataCard();
+  //   this.setState({
+  //     cardData: [...sparkleCardData],
+  //   });
+
+  //   this.chartPlace();
+  // }
+
+  // chartPlace = () => {
+  //   var chartDom = document.getElementById("topsaleDonut");
+  //   var myChart = echarts.init(chartDom);
+  //   var option;
+  //   option = saleGaugeOption;
+
+  //   option && myChart.setOption(option);
+  // };
+  // async loadDataCard() {
+  //   const { cardData } = this.state;
+  //   var allCardData = cardData;
+  //   cardData.map((data, i) => {
+  //     var uData = [];
+  //     data.sparklineData.data.map((d, j) => {
+  //       uData[j] = Math.floor(Math.random() * 10) + 1;
+  //     });
+  //     allCardData[i].sparklineData.data = [...uData];
+  //   });
+  //   this.setState({ cardData: [...allCardData] });
+  // }
+
+  render() {
+    const { loadingPage } = this.props;
+    const { cardData } = this.state;
+    if (loadingPage) {
+      return (
+        <div className="page-loader-wrapper">
+          <div className="loader">
+            <div className="m-t-30">
+              <img src={LogoiCON} width="48" height="48" alt="Lucid" />
+            </div>
+            <p>Please wait...</p>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div
+        onClick={() => {
+          document.body.classList.remove("offcanvas-active");
+        }}
+      >
+        <div>
+          <div className="container-fluid">
+            <PageHeader
+              HeaderText="Dashboard"
+              Breadcrumb={[{ name: "Dashboard" }]}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  
+  }
+}
+
+const mapStateToProps = ({
+  loginReducer,
+  navigationReducer,
+  analyticalReducer,
+}) => ({
+  email: loginReducer.email,
+  menuArrowToggle: navigationReducer.menuArrowToggle,
+  sparkleCardData: analyticalReducer.sparkleCardData,
+  topProductDropDown: analyticalReducer.topProductDropDown,
+  referralsDropDown: analyticalReducer.referralsDropDown,
+  recentChatDropDown: analyticalReducer.recentChatDropDown,
+  facebookShowProgressBar: analyticalReducer.facebookShowProgressBar,
+  twitterShowProgressBar: analyticalReducer.twitterShowProgressBar,
+  affiliatesShowProgressBar: analyticalReducer.affiliatesShowProgressBar,
+  searchShowProgressBar: analyticalReducer.searchShowProgressBar,
+  loadingPage: analyticalReducer.loadingPage,
+});
+
+export default connect(mapStateToProps, {
+  toggleMenuArrow,
+  loadSparcleCard,
+  onPressTopProductDropDown,
+  onPressReferralsDropDown,
+  onPressRecentChatDropDown,
+  onPressDataManagedDropDown,
+  facebookProgressBar,
+  twitterProgressBar,
+  affiliatesProgressBar,
+  searchProgressBar,
+})(Dashbord);
